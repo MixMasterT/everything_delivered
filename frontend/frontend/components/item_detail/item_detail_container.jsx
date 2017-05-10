ead616e57d5d295328c6c7bca2c907092b3e2af3
Nick Vizzutti <njvizzutti@gmail.com>
2017-05-09 17:21:29 -0700

    list component unstyled

--- frontend/frontend/components/item_detail/item_detail_container.jsx
+++ frontend/frontend/components/item_detail/item_detail_container.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ItemDetail from './item_detail.jsx';
 
-const mapStateToProps = (state, ownProps) => ({
-  // your code here...
-});
+const mapStateToProps = (state, ownProps) => {
+  let items = [{name: 'ItemName', _itemId: 'Recepient', vid: 'VendorId', url: 'imageurl', price: 19.99, id: 1 },
+{name: 'ItemName' vid: 'VendorId', url: 'imageurl', price: 19.99, id: 2 },
+{name: 'ItemName' vid: 'VendorId', url: 'imageurl', price: 19.99, id: 3 },
+{name: 'ItemName' vid: 'VendorId', url: 'imageurl', price: 19.99, id: 4 },
+{name: 'ItemName' vid: 'VendorId', url: 'imageurl', price: 19.99, id: 5 },
+{name: 'ItemName' vid: 'VendorId', url: 'imageurl', price: 19.99, id: 6 },
+{name: 'ItemName' vid: 'VendorId', url: 'imageurl', price: 19.99, id: 7 }];
+let obj = items.filter(item => item.id = ownProps.params.id)[0];
+console.log(obj);
+return {item: obj}
+}
 
 const mapDispatchToProps = dispatch => ({
   // your code here...
--- frontend/frontend/components/list_item/list_item.jsx
+++ frontend/frontend/components/list_item/list_item.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ListItem = (props) => (
-  <h3>List item</h3>
-);
+const ListItem = ({ item }) => {
+  let {id, url, name, price} = item;
+  return (
+    <li>
+      <Link to={`items/${id}`} >
+        <img src={url} />
+      </Link>
+      <h3>{name}</h3>
+      <h3>{price}</h3>
+    </li>
+
+  )
+  };
 
 export default ListItem;
