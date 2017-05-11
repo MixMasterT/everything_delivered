dd4958fad3b913763ba8f5c496cbaf1181df455a
Taylor Fitzgerald Chesnutt <taylor.chesnutt@gmail.com>
2017-05-11 11:47:41 -0700

    vendor seed data added

--- backend/controllers/orders_api.js
+++ backend/controllers/orders_api.js
@@ -20,20 +20,21 @@ exports.post = function(req, res) {
   }).save();
 }
 
+// returns all orders
 exports.list = function(req, res) {
   Orders.find(function(err, orders) {
     res.send(orders);
   });
 }
 
-// first locates a order by user.
+// returns all orders made by a user
 exports.showByUser = function(req, res) {
   Orders.findById({_uId: req.params._uId}, function(error, orders) {
     res.send(orders);
   });
 };
 
-//
+// returns all orders made by a driver (open and completed)
 exports.showByDriver = function(req, res) {
   Orders.findById({_dId: req.params._dId}, function(error, orders) {
     res.send(orders);
