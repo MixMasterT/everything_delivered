5b9761946fc68cd0262a2e7642d5e46b453ac705
Taylor Fitzgerald Chesnutt <taylor.chesnutt@gmail.com>
2017-05-18 14:13:10 -0700

    initial authy integration

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,17 +1,74 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
+const config = require('../config');
+const authy = require('authy')(config.authyKey);
+console.log(authy);
 
-var userSchema = new Schema({
+var UserSchema = new Schema({
     _userid:  {type: Schema.Types.ObjectId},
     phoneNumber: {type: String},
+    countryCode: {type: String},
     name: {type: String},
     address: {type: String},
+    authyId: {type: String},
     loc: {
       type: {type: String},
       coordinates: [Number],  // [<longitude>, <latitude>]
     },
 });
 
-userSchema.index({loc: '2dsphere'});
+UserSchema.index({loc: '2dsphere'});
 
-module.exports = mongoose.model('User', userSchema);
+UserSchema.methods.sendAuthyToken = function(cb) {
+    const self = this;
+
+    if (!self.authyId) {
+        // Register this user if it's a new user
+        authy.register_user(self.email, self.phoneNumber, self.countryCode,
+            function(err, response) {
+            if (err || !response.user) return cb.call(self, err);
+            self.authyId = response.user.id;
+            self.save(function(err, doc) {
+                if (err || !doc) return cb.call(self, err);
+                self = doc;
+                sendToken();
+            });
+        });
+    } else {
+        // Otherwise send token to a known user
+        sendToken();
+    }
+
+    // With a valid Authy ID, send the 2FA token for this user
+    function sendToken() {
+        authy.request_sms(self.authyId, true, function(err, response) {
+            cb.call(self, err);
+        });
+    }
+};
+
+UserSchema.methods.verifyAuthyToken = function(otp, cb) {
+    const self = this;
+    console.log(authy);
+    authy.verify(self.authyId, otp, function(err, response) {
+        cb.call(self, err, response);
+    });
+};
+// sends any message me want
+UserSchema.methods.sendMessage = function(message, successCb, errorCb) {
+      const self = this;
+      const toNumber = `+${self.countryCode}${self.phoneNumber}`;
+
+      twilioClient.messages.create({
+          to: toNumber,
+          from: config.twilioNumber,
+          body: message,
+      }).then(function() {
+        successCb();
+      }).catch(function(err) {
+        errorCb(err);
+      });
+  };
+
+
+module.exports = mongoose.model('User', UserSchema);
