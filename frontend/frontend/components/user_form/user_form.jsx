15f946ff954093b490be20d1d34f13b3f5df568d
mixmastert <torahlight@gmail.com>
2017-05-19 15:41:26 -0700

    make a few changes to the UserForm component

--- frontend/frontend/components/user_form/user_form.jsx
+++ frontend/frontend/components/user_form/user_form.jsx
@@ -3,25 +3,39 @@ import React from 'react';
 class UserForm extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      address: "",
+      name: "",
+      phone: "",
+    }
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit() {
-    //dispatch actions
+  handleSubmit(e) {
+    e.preventDefault();
+    // verify phone number, send error if not valid
+
+    // verify address, send error if not valid
+
+    // set user
+    this.props.receiveUser(this.state);
   }
 
-  handleChange() {
-    //set field
+  handleChange(field) {
+    e => this.setState({ [field]: e.target.value })
   }
 
   render() {
     return (
       <div className="user-form">
         <form onSubmit={ handleSubmit }>
-          <input type='text' onChange={() => handleChange('name')} />
-          <input type='text' onChange={() => handleChange('address')} />
-          <input type='text' onChange={() => handleChange('phone')} />
+          <input type='text' onChange={handleChange('name')} />
+          <input type='text' onChange={handleChange('address')} />
+          <input type='text' onChange={handleChange('phone')} />
+          <input type='submit' value='save' />
         </form>
       </div>
     );
