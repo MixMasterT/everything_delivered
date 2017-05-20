574e6eaaed1dc31830c3bb36385cbbb8a6f88f83
Taylor Fitzgerald Chesnutt <taylor.chesnutt@gmail.com>
2017-05-20 10:44:14 -0700

    merge conflict fixed, hopefully

---
+++ frontend/frontend/components/user_form/user_form_container.jsx
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import UserForm from './user_form.jsx';
+import {receiveUser, setAddress, setName, setPhone } from '../../actions/user_actions';
+
+const mapStateToProps = (state, ownProps) => ({
+  // your code here...
+});
+
+const mapDispatchToProps = dispatch => ({
+  receiveUser: (user) => dispatch(receiveUser(user)),
+  setAddress: (addr) => dispatch(setAddress(addr)),
+  setName: (name) => dispatch(setName(name)),
+  setPhone: (phone) => dispatch(setPhone(phone))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserForm);
--- frontend/frontend/util/item_api_util.js
+++ frontend/frontend/util/item_api_util.js
@@ -1,8 +1,10 @@
+const baseUrl = 'https://localhost:3000/';
+
 export const fetchAllItems = () => {
   return new Promise((resolve, reject) => {
     var request = new XMLHttpRequest();
 
-    request.open('GET', 'https://testproject-api.strv.com/events');
+    request.open('GET', `${baseUrl}`);
 
     request.onreadystatechange = function () {
       if (this.readyState === 4) {
@@ -31,7 +33,7 @@ export const fetchSingleItem = (itemId) => {
   return new Promise((resolve, reject) => {
     var request = new XMLHttpRequest();
 
-    request.open('GET', `https://testproject-api.strv.com/events/${itemId}`);
+    request.open('GET', `${baseUrl}${itemId}`);
 
     request.onreadystatechange = function () {
       if (this.readyState === 4) {
