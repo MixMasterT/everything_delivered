00374fe719853a01fec0a14438a3f445265cd760
Nick Vizzutti <njvizzutti@gmail.com>
2017-05-31 19:09:11 -0700

    same error handling added to vendor_api

--- backend/controllers/vendor_api.js
+++ backend/controllers/vendor_api.js
@@ -2,12 +2,22 @@ var Vendor = require('../models/vendor.js');
 
 exports.listAll = function(req, res) {
   Vendor.find(function(err, items) {
-    res.send(vendors);
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      res.send(vendors);
+    }
   });
 }
 
 exports.listOne = function(req, res) {
   Vendor.find({_id: req.params._id}, function(err, item) {
-    res.send(vendor);
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      res.send(vendor);
+    }
   });
 }
